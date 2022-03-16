f3156851905ffd90317ca82dd6668ffe3e4d46d6
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-03-16 14:35:30 +0545

    Handling Route parameter

--- basicRestAPI/main.go
+++ basicRestAPI/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,8 +26,45 @@ func main() {
 		r1.DELETE("/hello", deleteHello)
 	}
 
+	// handling path params
+	r.GET("/product/:id", getProductByID)
+	r.GET("/profile/:username", showProfile)
+	r.GET("/compute/:num1/add/:num2", compute)
+
 	r.Run() //Default port 8080
 	fmt.Println("Server is running!!")
+}
+
+func getProductByID(c *gin.Context) {
+	id := c.Param("id")                   //string
+	idn, _ := strconv.ParseInt(id, 10, 0) //convert sting to integer
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"id":   idn,
+		"name": "Product A",
+	})
+}
+
+func showProfile(c *gin.Context) {
+	username := c.Param("username") //string
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"username": username,
+		"name":     "Doe Family",
+	})
+}
+
+func compute(c *gin.Context) {
+	num1 := c.Param("num1") //string
+
+	num1Int, _ := strconv.ParseInt(num1, 10, 0)            //convert sting to integer
+	num2Int, _ := strconv.ParseInt(c.Param("num2"), 10, 0) //convert sting to integer
+	result := num1Int + num2Int
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"num1":   num1Int,
+		"num2":   num2Int,
+		"result": result,
+	})
 }
 
 func getHello(c *gin.Context) {
